6c0a298fcf886785eb0e537ecbe532dfd9d705ab
Warner <Amsu.Warner@lfg.com>
2023-08-09 09:32:04 -0400

    Spacing

--- 04-angular/bug-tracker-app/src/app/projects/services/projects.service.ts
+++ 04-angular/bug-tracker-app/src/app/projects/services/projects.service.ts
@@ -11,7 +11,7 @@ export class ProjectsService {
 
   constructor(private projectApi : ProjectsApiService) { }
 
-  load(){
+  load() {
     this.projectApi
       .load()
       .subscribe(projects => this.projects = projects);
