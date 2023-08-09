0d34dfd43f8ea4fcda24a79276bcd1570e278c4b
Warner <Amsu.Warner@lfg.com>
2023-08-09 14:22:14 -0400

    Logout functionality

--- 04-angular/ang-intro/src/app/app.component.ts
+++ 04-angular/ang-intro/src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app/component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   //state (data)
   title = 'ang-intro';
   userChoice : string = '';
   
-  constructor() {
+  constructor(public authService: AuthService) {
     setTimeout(() => {
       this.title = 'New-Ang-Intro'
     }, 5000);
+  }
+  onBtnLogoutClick() {
+    this.authService.logout()
   }
 }
