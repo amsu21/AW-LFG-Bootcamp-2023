05f18864d3c0e8d375743ff028b1ca571ad05f76
Warner <Amsu.Warner@lfg.com>
2023-08-09 09:34:05 -0400

    Fixed import

--- 04-angular/bug-tracker-app/src/app/bugs/bugs-router.module.ts
+++ 04-angular/bug-tracker-app/src/app/bugs/bugs-router.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { BugsComponent } from "./bugs.component";
+import { BugDetailsComponent } from "./components/bug-details/bug-details.component";
 
 const routes : Routes = [
     {
         path : "bugs", 
         component : BugsComponent,
-        /* children : [
-            { path: "create", component: BugEditComponent }
-        ] */
+         children : [
+            { path: "create", component: BugDetailsComponent }
+        ] 
     }
 ]
 @NgModule({
