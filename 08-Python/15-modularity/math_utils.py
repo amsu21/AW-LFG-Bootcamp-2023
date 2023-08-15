a763d5d3e6c79b71c28602ee5e70a827673dffb8
Warner <Amsu.Warner@lfg.com>
2023-08-15 16:22:51 -0400

    New folders

---
+++ 08-Python/15-modularity/math_utils.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+if(__name__ == "__main__"):
+    print(add(500, 600))
+    print(subtract(500, 600))
---
+++ 08-Python/15-modularity/utils/string_utils.py
@@ -0,0 +1,2 @@
+def legnth(str):
+    return 
