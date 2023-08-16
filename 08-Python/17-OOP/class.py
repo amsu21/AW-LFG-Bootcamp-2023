8e25471c1f8d24dbd8288e180313f554e98331e3
Warner <Amsu.Warner@lfg.com>
2023-08-16 09:58:15 -0400

    More dunder init

--- 08-Python/17-OOP/class.py
+++ 08-Python/17-OOP/class.py
@@ -1,7 +1,18 @@
 class Employee:
-    def __init__(self):
-        print("A new employee is being created")
-        self.first_name = "Amsu"
-        self.last_name = "Warner"
+    # def __init__(self):
+    #     print("A new employee is being created")
+    #     self.first_name = "Amsu"
+    #     self.last_name = "Warner"
+    
+    # def __init__(self, fName, lName):
+    #     print("A new employee is being created")
+    #     self.first_name = fName
+    #     self.last_name_name = lName
+    
+    # OPTIONAL VALUES
+    def __init__(self, fName="", lName=""):
+        print("New employee is being created")
+        self.first_name = fName
+        self.last_name = lName
 
 emp = Employee()
