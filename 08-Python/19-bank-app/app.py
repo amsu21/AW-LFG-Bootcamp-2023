3483743900f68bdb6f3714f78fa17abb990d314e
Warner <Amsu.Warner@lfg.com>
2023-08-17 14:49:11 -0400

    Bank App

---
+++ 08-Python/19-bank-app/app.py
@@ -0,0 +1,9 @@
+from csvstorage import CSVStorage
+from bank import BankAccount
+from menu import Menu
+
+if __name__ == '__main__':
+    storage = CSVStorage('transactions.csv')
+    account = BankAccount('Magesh', storage)
+    menu = Menu(account)
+    menu.run()
