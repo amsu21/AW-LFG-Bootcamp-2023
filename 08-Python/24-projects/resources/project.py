b23df4d5f1392e964abdf53862bb6e35d08655dc
Warner <Amsu.Warner@lfg.com>
2023-08-28 14:16:54 -0400

    Added Project class and GET & PUT method

--- 08-Python/24-projects/resources/project.py
+++ 08-Python/24-projects/resources/project.py
@@ -17,5 +17,22 @@ project_parser.add_argument('title',
                             required=True,
                             help='Invalid Data'
                             )
-                            
+
+project_parser.add_argument('createdAt',
+                            type=inputs.datetime_from_rfc822,
+                            required=True,
+                            help='Invalid Data'
+                            )
+
+class Project(Resource):
+    def get(slef, id):
+        project_from_db = ProjectModel.get_by_id(id)
+        return jsonify(project_from_db.to_json())
+    
+    def put(self, id):
+        project_to_update = project_parser.parse_args(id)
+        project_from_db = ProjectModel.get_by_id(id)
+        project_from_db.title = project_to_update['title']
+        project_from_db.save()
+        return jsonify(project_from_db.to_json())                           
                             
