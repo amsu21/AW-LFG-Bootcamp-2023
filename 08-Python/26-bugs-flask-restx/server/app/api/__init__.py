fdc9dc48815aea120c73a5f88784ce8560a7f454
Warner <Amsu.Warner@lfg.com>
2023-08-30 09:31:30 -0400

    Docker folder and pycache

--- 08-Python/26-bugs-flask-restx/server/app/api/__init__.py
+++ 08-Python/26-bugs-flask-restx/server/app/api/__init__.py
@@ -1,2 +1,6 @@
 from flask_restx import Api
 
+from app.api.bugs.views import bugs_namespace
+
+api = Api(version="1.0", title="Flask API", doc="/api/v1/docs")
+api.add_namespace(bugs_namespace, path="/api/v1/bugs")
